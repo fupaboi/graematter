80e3d2e1d98b57e826ff32c5957b00ff1016e9a3
fupaboi <74823324+fupaboi@users.noreply.github.com>
2020-11-21 20:42:25 +0530

    Update data.js

--- data.js
+++ data.js
@@ -3,10 +3,10 @@ var ctx = document.getElementById("myChart").getContext("2d");
 var myChart = new Chart(ctx, {
 	type:'line',
 	data:{
-		labels: ['Jan','Feb','March','April','May'],
+		labels: ['Jan','Feb','March','April','May','June'],
 		datasets: [
 		{
-			data:[10,20,12,23,19],
+			data:[10,20,12,23,19,34],
 			borderWidth:4,
 			borderColor:'rgba(0,255,0,0.2)',
 			backgroundColor: 'rgba(0,255,0,0.1)'
