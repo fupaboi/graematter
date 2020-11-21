e3699230918db49d1ed451a01146fce7e4c4836b
fupaboi <74823324+fupaboi@users.noreply.github.com>
2020-11-21 19:07:37 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,22 @@
+var ctx = document.getElementById("myChart").getContext("2d");
+
+var myChart = new Chart(ctx, {
+	type:'line',
+	data:{
+		labels: ['Jan','Feb','March'],
+		datasets: [
+		{
+			data:[10,20,12],
+			borderWidth:4,
+			borderColor:'rgba(0,255,0,0.2)',
+			backgroundColor: 'rgba(0,255,0,0.1)'
+		},
+	    ],
+	  },
+
+    options: {
+      legend: {
+         display: false
+      },
+     },	
+	});
