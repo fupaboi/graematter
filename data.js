12ee86f677b15910d6b3dfd3c522247d78510e98
fupaboi <74823324+fupaboi@users.noreply.github.com>
2020-11-28 19:12:40 +0530

    Update data.js

--- data.js
+++ data.js
@@ -3,13 +3,27 @@ var ctx = document.getElementById("myChart").getContext("2d");
 var myChart = new Chart(ctx, {
 	type:'line',
 	data:{
-		labels: ['Jan','Feb','March','April','May','June'],
+		labels: ['','November','','','',
+		         '','','','','',
+		         '','',''],
 		datasets: [
 		{
-			data:[10,20,12,23,19,34],
+			data:[,,,,,
+			      ,,,,,
+			      ,,],
 			borderWidth:4,
 			borderColor:'rgba(0,255,0,0.2)',
-			backgroundColor: 'rgba(0,255,0,0.1)'
+			backgroundColor:'rgba(0,255,0,0.1)',
+
+		},
+		{
+			data:[-24.88,-24.88,-26.21,-25,-16.2,
+			      -15.75,-15.76,-15.79,-22.3,-21.3,
+			      -22.8,-22.7,-26.4],
+			borderWidth:4,
+			borderColor:'rgba(255,0,0,0.2)',
+			backgroundColor:'rgba(255,0,0,0.2)',
+
 		},
 	    ],
 	  },
@@ -18,5 +32,29 @@ var myChart = new Chart(ctx, {
       legend: {
          display: false
       },
+      scales: {
+      	xAxes: [{
+      		ticks: {
+      			fontSize: 15,
+      			fontFamily: 'Montserrat'
+      		}
+      	}],
+      	yAxes: [{
+      		ticks: {
+      			fontSize: 15,
+      			fontFamily: 'Montserrat'      			
+      		},
+            afterFit: function(scale) {
+               scale.width = 130 //<-- set value as you wish 
+            },
+            scaleLabel: {
+            	display: true,
+            	labelString: 'Equity %',
+            	fontSize: 30,
+            	//fontStyle: 'bold',
+      			fontFamily: 'Montserrat'            	
+            },
+      	}]
+      }
      },	
 	});
