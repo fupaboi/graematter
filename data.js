3377e72869e92c4f084e779628f5a599cb0f74bb
fupaboi <74823324+fupaboi@users.noreply.github.com>
2020-12-08 16:58:30 +0530

    Update data.js

--- data.js
+++ data.js
@@ -39,7 +39,7 @@ var myChart = new Chart(ctx, {
          display: false
       },
       animation: {
-	  duration: 3500,    
+	  duration: 5000,    
       },	    
       scales: {
       	xAxes: [{
@@ -122,7 +122,7 @@ var my_bar_chart = new Chart(ct_bar, {
    },
     options: {
       animation: {
-	  duration: 3500,    
+	  duration: 5000,    
       },	    
       legend: {
          display: false
