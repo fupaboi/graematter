f236c615878fac4426658c3fe194fb28ea410d79
fupaboi <74823324+fupaboi@users.noreply.github.com>
2020-12-17 18:08:35 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,99 +1,100 @@
 var ctx = document.getElementById("myChart").getContext("2d");
 
 var myChart = new Chart(ctx, {
-	type:'line',
-	data:{
-		labels: ['','Nov','17','18','19',
-		         '20','21','22','23','24',
-		         '25','26','27','28','29',
-                         '30','Dec','2','3','4',
-			 '5','6','7','8','9',
-			 '10','11','12','13','14',
-			 '15'],
-		datasets: [
-		{
-			data:[,,,,,
-			      ,,,,,
-			      ,,,,,
-                              ,,,,,
-			      ,,,,,
-			      ,,,,,],
-			borderWidth:1,
-			borderColor:'rgba(0,255,0,0.5)',
-			backgroundColor:'rgba(0,255,0,0.1)',
+   type:'line',
+   data:{
+      labels: ['Nov 15','Nov 16','Nov 17','Nov 18','Nov 19',
+               'Nov 20','Nov 21','Nov 22','Nov 23','Nov 24',
+               'Nov 25','Nov 26','Nov 27','Nov 28','Nov 29',
+               'Nov 30','Dec 1','Dec 2','Dec 3','Dec 4',
+               'Dec 5','Dec 6','Dec 7','Dec 8','Dec 9',
+               'Dec 10','Dec 11','Dec 12','Dec 13','Dec 14',
+               'Dec 15','Dec 16'],
+      datasets: [
+      {
+         data:[,,,,,
+               ,,,,,
+               ,,,,,
+               ,,,,,
+               ,,,,,
+               ,,,,,
+               ,],
+         borderWidth:0,
+         borderColor:'rgba(0,255,0,0.5)',
+         backgroundColor:'rgba(0,255,0,0.1)',
 
-		},
-		{
-			data:[-24.88,-24.88,-26.21,-25,-16.2,
-			      -15.75,-15.76,-15.79,-22.3,-21.3,
-			      -22.8,-22.7,-26.4,-26.2,-26.3,
-                              -26.4,-25.4,-27.2,-45.4,-42.1,
-			      -42.1,-42.1,-44,-44,-42,
-			      -41,-30,-30,-30,-31,-33],
-			borderWidth:1,
-			borderColor:'rgba(255,0,0,0.5)',
-			backgroundColor:'rgba(255,0,0,0.1)',
+      },
+      {
+         data:[-24.88,-24.88,-26.21,-25,-16.2,
+               -15.75,-15.76,-15.79,-22.3,-21.3,
+               -22.8,-22.7,-26.4,-26.2,-26.3,
+                -26.4,-25.4,-27.2,-45.4,-42.1,
+               -42.1,-42.1,-44,-44,-42,
+               -41,-30,-30,-30,-31,
+               -33,-35],
 
-		},
-	    ],
-	  },
+         borderColor:'rgba(230,0,0,0.3)',
+         backgroundColor:'#FFFFFF',
+         pointRadius:0,
+         pointBackgroundColor:'rgba(230,0,0,0.3)',
+
+      },
+       ],
+     },
 
     options: {
+      tooltips:{
+        yAlign:'top',
+        displayColors:false,
+        mode:'x-axis',
+        titleAlign:'center',
+        bodyAlign:'center',
+        titleFontFamily:'Montserrat',
+        bodyFontFamily:'Montserrat',
+        titleFontSize:15,
+        bodyFontSize:15,
+        titleFontStyle:'bold',
+        titleFontColor:'rgba(0,0,0,0.5)',
+        bodyFontColor:'rgba(255,255,255,1)',
+        backgroundColor:'rgba(0,0,0,0.2)'
+      },
       legend: {
          display: false
       },
       animation: {
-	  duration: 3500,    
-      },	    
+      duration: 3500,    
+      },     
       scales: {
-      	xAxes: [{
-      		ticks: {
-      			fontSize: 15,
-      			fontStyle: 'bold',
-      			fontFamily: 'Montserrat',
-      			fontColor: '#A4A4A4' 
-      		},
-            afterFit: function(scale) {
-               scale.height = 100 //<-- set value as you wish 
+         xAxes: [{
+            gridLines: {
+                drawBorder: false,
+                display: false,
             },
-            scaleLabel: {
-            	display: true,
-            	labelString: 'date',
-            	fontSize: 30,
-            	fontStyle: 'bold',
-      			fontFamily: 'Montserrat',          	
-            },      		
-      	}],
-      	yAxes: [{
-      		ticks: {
-      			fontSize: 15,
-      			fontStyle: 'bold',
-      			fontFamily: 'Montserrat', 
-      			fontColor: '#A4A4A4'     			
-      		},
-            afterFit: function(scale) {
-               scale.width = 100 //<-- set value as you wish 
+            ticks: {
+               fontSize: 12,
+               fontStyle: 'bold',
+               fontFamily: 'Montserrat',
+               fontColor: 'white'
+            },        
+         }],
+         yAxes: [{
+            gridLines: {
+                drawBorder: false,
+                display: false,
             },
-            scaleLabel: {
-            	display: true,
-            	labelString: 'equity %',
-            	fontSize: 30,
-            	fontStyle: 'bold',
-      			fontFamily: 'Montserrat',          	
+            ticks: {
+               fontSize: 1,
+               fontColor: 'white'             
             },
-      	}]
+         }]
       },
-     },	
-	});
+     },  
+   });
 
 var ct_bar = document.getElementById("my_bar_chart").getContext("2d");
 
-var g_bor = 'rgba(0,255,0,0.7)'
-var r_bor = 'rgba(255,0,0,0.5)'
-
-var g_back = 'rgba(0,255,0,0.1)'
-var r_back = 'rgba(255,0,0,0.1)'
-
+var g_back= 'rgba(0,230,0,0.3)'
+var r_back= 'rgba(230,0,0,0.3)'
 
 var my_bar_chart = new Chart(ct_bar, {
    type:'bar',
@@ -102,79 +103,73 @@ var my_bar_chart = new Chart(ct_bar, {
                '11','12','13','14','15','16','17','18','19','20',
                '21','22','23','24','25','26','27','28','29','30',
                '31','32','33','34','35','36','37','38','39','40',
-	       '41','42','43','44','45','46','47','48','49','50',
-	       '51','52','53','54','55','56','57'],
+               '41','42','43','44','45','46','47','48','49','50',
+               '51','52','53','54','55','56','57','58','59'],
       datasets: [
       {
          data:[-2.61,-1.44,1.78,-1.08,-0.17,-1.30,-4.25,6.9,-1.35,-2.02,
                12.22,-2.76,-1.69,5.03,-4,-2.75,0.33,-2.33,3.41,-3.14,
                -3.06,-3.55,8.39,-2.92,-2.84,2.53,3.47,-2.53,-0.85,1.86,
                -0.49,4.43,-3.95,-2.2,-1.5,-2.6,-3.48,-2.95,-3.98,-3.6,
-	        1.69,1.73,-0.3,-1.1,3.27,-3.65,2.6,-0.1,2.62,-3.87,
-	        3.07,-0.55,-2.55,14.5,-2.39,-1.72,0.52],
-         borderWidth:1,
-         borderColor:[r_bor,r_bor,g_bor,r_bor,r_bor,r_bor,r_bor,g_bor,r_bor,r_bor,
-                      g_bor,r_bor,r_bor,g_bor,r_bor,r_bor,g_bor,r_bor,g_bor,r_bor,
-                      r_bor,r_bor,g_bor,r_bor,r_bor,g_bor,g_bor,r_bor,r_bor,g_bor,
-                      r_bor,g_bor,r_bor,r_bor,r_bor,r_bor,r_bor,r_bor,r_bor,r_bor,
-		      g_bor,g_bor,r_bor,r_bor,g_bor,r_bor,g_bor,r_bor,g_bor,r_bor,
-		      g_bor,r_bor,r_bor,g_bor,r_bor,r_bor,g_bor],
+               1.69,1.73,-0.3,-1.1,3.27,-3.65,2.6,-0.1,2.62,-3.87,
+               3.07,-0.55,-2.55,14.5,-2.39,-1.72,0.52,-3.03,1.12],
+         borderWidth:0,
          backgroundColor:[r_back,r_back,g_back,r_back,r_back,r_back,r_back,g_back,r_back,r_back,
                           g_back,r_back,r_back,g_back,r_back,r_back,g_back,r_back,g_back,r_back,
                           r_back,r_back,g_back,r_back,r_back,g_back,g_back,r_back,r_back,g_back,
                           r_back,g_back,r_back,r_back,r_back,r_back,r_back,r_back,r_back,r_back,
-			  g_back,g_back,r_back,r_back,g_back,r_back,g_back,r_back,g_back,r_back,
-			  g_back,r_back,r_back,g_back,r_back,r_back,g_back],
+                          g_back,g_back,r_back,r_back,g_back,r_back,g_back,r_back,g_back,r_back,
+                          g_back,r_back,r_back,g_back,r_back,r_back,g_back,r_back,g_back],
       },
       ],
    },
     options: {
-      animation: {
-	  duration: 3500,    
-      },	    
+      tooltips:{
+        yAlign:'top',
+        displayColors:false,
+        mode:'x-axis',
+        titleAlign:'center',
+        bodyAlign:'center',
+        titleFontFamily:'Montserrat',
+        bodyFontFamily:'Montserrat',
+        titleFontSize:15,
+        bodyFontSize:15,
+        titleFontStyle:'bold',
+        titleFontColor:'rgba(0,0,0,0.5)',
+        bodyFontColor:'rgba(255,255,255,1)',
+        backgroundColor:'rgba(0,0,0,0.2)'
+      },
+     animation: {
+     duration: 3500,    
+      },     
       legend: {
          display: false
       },
       scales: {
          xAxes: [{
             gridLines: {
-               display:false
-            }, 		 
+                drawBorder: false,
+                display: false,
+            },
+
             ticks: {
-               fontSize: 15,
+               fontSize: 0,
                fontStyle: 'bold',
                fontFamily: 'Montserrat',
-               fontColor: '#A4A4A4' 
+               fontColor: 'black' 
             },
-            afterFit: function(scale) {
-               scale.height = 100 //<-- set value as you wish 
-            },
-            scaleLabel: {
-               display: true,
-               labelString: 'trade number',
-               fontSize: 30,
-               fontStyle: 'bold',
-               fontFamily: 'Montserrat',           
-            },         
          }],
          
          yAxes: [{
+            gridLines: {
+                drawBorder: false,
+            },
+
             ticks: {
-               fontSize: 15,
-               fontStyle: 'bold',
-               fontFamily: 'Montserrat', 
-               fontColor: '#A4A4A4'             
+               fontSize: 1, 
+               fontColor: 'white'            
             },
-            afterFit: function(scale) {
-               scale.width = 100 //<-- set value as you wish 
-            },
-            scaleLabel: {
-               display: true,
-               labelString: 'trade result in %',
-               fontSize: 30,
-               fontStyle: 'bold',
-               fontFamily: 'Montserrat',           
-            },
+
          }],
       },
    },   
