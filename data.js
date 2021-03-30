034f333ddf9345ab807faddac00ee3e5aff1983c
fupaboi <74823324+fupaboi@users.noreply.github.com>
2021-03-30 10:30:52 +0530

    Update data.js

--- data.js
+++ data.js
@@ -3,6 +3,7 @@ var ctx = document.getElementById("myChart").getContext("2d");
 var myChart = new Chart(ctx, {
    type:'line',
    data:{
+
       labels: ['Nov 15','Nov 16','Nov 17','Nov 18','Nov 19',
                'Nov 20','Nov 21','Nov 22','Nov 23','Nov 24',
                'Nov 25','Nov 26','Nov 27','Nov 28','Nov 29',
@@ -29,7 +30,8 @@ var myChart = new Chart(ctx, {
                'Mar 10','Mar 11','Mar 12'],
       
       datasets: [
-      {
+      {  
+         label: 'Equity %',
          data:[-24.88,-24.88,-26.21,-25,-16.2,
                -15.75,-15.76,-15.79,-22.3,-21.3,
                -22.8,-22.7,-26.4,-26.2,-26.3,
@@ -55,8 +57,8 @@ var myChart = new Chart(ctx, {
                -29,-30,-30,-32,-35,
                -41,-41,-10],
 
-         borderColor:'rgba(70,255,237,0.3)',
-         backgroundColor:'#232323',
+         borderColor:'rgba(255,0,0,1)',
+         borderWidth:2,
          pointRadius:0,
 
       },
@@ -64,129 +66,62 @@ var myChart = new Chart(ctx, {
      },
 
     options: {
+      
+      scales: {
+        yAxes: [{
+          ticks: {
+            display: false
+                 }
+               }],
+
+        xAxes: [{
+          ticks: {
+            display: false
+                 }
+               }]
+              },
 
       tooltips:{
-        yAlign:'top',
-        displayColors:false,
         mode:'x-axis',
-        titleAlign:'center',
-        bodyAlign:'center',
-        titleFontFamily:'Montserrat',
-        bodyFontFamily:'Montserrat',
-        titleFontSize:15,
-        bodyFontSize:15,
-        titleFontStyle:'bold',
-        titleFontColor:'rgba(70,255,237,0.8)',
-        bodyFontColor:'rgba(255,255,255,0.8)',
-        backgroundColor:'rgba(0,0,0,0.5)'
-      },
+        backgroundColor:'rgba(255,0,0,0.5)',
+        displayColors:false
+              },
+            },  
+});
+    
+var ctx_bar = document.getElementById("myChart_bar").getContext("2d");
 
-      legend: {
-         display: false
-      },
-
-      animation: {
-      duration: 3500,    
-      }, 
-
-      scales: {
-
-         xAxes: [{
-            gridLines: {
-                drawBorder: false,
-                display: false,
-            },
-            ticks: {
-               fontSize: 0,
-            },        
-         }],
-
-         yAxes: [{
-            gridLines: {
-                drawBorder: false,
-                display: false,
-            },
-            ticks: {
-               fontSize: 0,          
-            },
-         }]
-      },
-     },  
-   });
-
-var ct_bar = document.getElementById("my_bar_chart").getContext("2d");
-
-var my_bar_chart = new Chart(ct_bar, {
+var myChart_bar = new Chart(ctx_bar, {
    type:'bar',
-   data:{
-      labels: ['1','2','3','4','5','6','7','8','9','10',
-               '11','12','13','14','15','16','17','18','19','20',
-               '21','22','23','24','25','26','27','28','29','30',
-               '31','32','33','34','35','36','37','38','39','40',
-               '41','42','43','44','45','46','47','48','49','50',
-               '51','52','53','54','55','56','57','58','59','60',
-               '61','62','63','64','65','66','67','68','69','70',
-               '71','72','73','74','75','76','77','78'],
-      datasets: [
-      {
-         data:[-2.61,-1.44,1.78,-1.08,-0.17,-1.30,-4.25,6.9,-1.35,-2.02,
-               12.22,-2.76,-1.69,5.03,-4,-2.75,0.33,-2.33,3.41,-3.14,
-               -3.06,-3.55,8.39,-2.92,-2.84,2.53,3.47,-2.53,-0.85,1.86,
-               -0.49,4.43,-3.95,-2.2,-1.5,-2.6,-3.48,-2.95,-3.98,-3.6,
-               1.69,1.73,-0.3,-1.1,3.27,-3.65,2.6,-0.1,2.62,-3.87,
-               3.07,-0.55,-2.55,14.5,-2.39,-1.72,0.52,-3.03,1.12,5.68,
-               0.85,-4,0,4.67,30.16,4.1,0.73,-2.53,3.09,1.89,
-              -2.61,-3.03,4.1,-2.33,2.34,-4.82,-2.79,4.85],
-         borderWidth:0,
-         backgroundColor:'rgba(70,255,237,0.3)',
-      },
-      ],
-   },
-    options: {
-      tooltips:{
-        yAlign:'bottom',
-        displayColors:false,
-        mode:'x-axis',
-        titleAlign:'center',
-        bodyAlign:'center',
-        titleFontFamily:'Montserrat',
-        bodyFontFamily:'Montserrat',
-        titleFontSize:15,
-        bodyFontSize:15,
-        titleFontStyle:'bold',
-        titleFontColor:'rgba(70,255,237,0.8)',
-        bodyFontColor:'rgba(255,255,255,0.8)',
-        backgroundColor:'rgba(0,0,0,0.5)'
-      },
-     animation: {
-     duration: 3500,    
-      },     
-      legend: {
-         display: false
-      },
+   data: {
+    labels:[1,2,3,4,5,6,7,8,9,10,9,8,7,6,5,4,3,2,1],
+    datasets:[
+    {
+      data:[1,2,-3,4,5,6,7,8,9,10,9,8,7,6,5,4,3,2,1],
+      label:"trade wise pnl %",
+      backgroundColor:'rgba(0,255,255,1)',
+    },
+    ],
+   },     
+     options: {
+      
       scales: {
-         xAxes: [{
-            gridLines: {
-                drawBorder: false,
-                display: false,
-            },
+        yAxes: [{
+          ticks: {
+            display: false
+                 }
+               }],
 
-            ticks: {
-               fontSize: 0,
-            },
-         }],
-         
-         yAxes: [{
-            gridLines: {
-                drawBorder: false,
-                display: false,
-            },
+        xAxes: [{
+          ticks: {
+            display: false
+                 }
+               }]
+              },
 
-            ticks: {
-               fontSize: 0,          
-            },
-
-         }],
-      },
-   },   
+      tooltips:{
+        mode:'x-axis',
+        displayColors:false
+              },
+            },  
 });
